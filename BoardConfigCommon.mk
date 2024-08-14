4ce05e0aa9b882159522f8cef1c9e3eb69cfa638
chaptsand <chaptsand@gmail.com>
2024-08-14 13:47:47 +0800

    sm8550-common: Switch to common QTI bt component
    Change-Id: I6f54a460b342f08fd15e75efc0f0128b40afe04f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,7 +114,6 @@ BOARD_ROOT_EXTRA_FOLDERS := \
 
 # Properties
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 
